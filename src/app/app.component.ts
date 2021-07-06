dd5571d9801ee4091d3d7f798cedca609626a89e
namratapdr <namratapodder138@gmail.com>
2021-07-07 01:44:30 +0530

    Finished: 2. Understanding Angular Template Syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-gen';
+
+  length=0;
+  password="";
+  isUseLetters=false;
+  isUseNumbers=false;
+  isUseSymbols=false;
+
+  onChangeLength(event: Event){
+    console.log("EVENT =>",event)
+    
+    if(event){
+      const targetElement: HTMLInputElement = <HTMLInputElement> event.target;
+
+      const value: number = parseInt(targetElement.value);
+      if (!isNaN(value))
+      this.length=value;
+    }
+    
+  }
+  onChangeUseLetters(){
+    this.isUseLetters = !this.isUseLetters;
+  }
+  onChangeUseNumbers(){
+    this.isUseNumbers = !this.isUseNumbers;
+  }
+  onChangeUseSymbols(){
+    this.isUseSymbols = !this.isUseSymbols;
+  }
+
+  onButtonClick(){
+    const numbers ='1234567890';
+    const letters = 'abcdefghijklmopqrstuvwxyz';
+    const symbols= '!@#$%^&*()';
+
+    let validChars="";
+
+    if(this.isUseLetters){
+        validChars += letters;
+    }
+    if(this.isUseNumbers){
+      validChars +=numbers ;
+    }
+    if(this.isUseSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = "";
+
+    for (let i=0; i<=this.length;i++)
+    {
+      let index= Math.floor(Math.random()* validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password=generatedPassword;
+  }
 }
